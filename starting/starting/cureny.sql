04522c82ca9b79cb2fc5d73483d74a34d144d5d2
madhureddyr <64367985+madhureddyr@users.noreply.github.com>
2020-10-07 21:11:15 +0530

    Update cureny.sql

--- starting/starting/cureny.sql
+++ starting/starting/cureny.sql
@@ -2,5 +2,6 @@
 (
 	[curency_code] CHAR(3) NOT NULL PRIMARY KEY, 
     [curency_name] NVARCHAR(50) NULL, 
-    [curency_description] NVARCHAR(50) NULL
+    [curency_description] NVARCHAR(50) NULL,
+     [curency_description1] NVARCHAR(50) NULL
 )
